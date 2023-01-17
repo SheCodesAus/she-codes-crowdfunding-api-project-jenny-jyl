d2f0ca1514652d0a297ed5fcd2ea285df9859e7c
jenny-jyl <114784070+jenny-jyl@users.noreply.github.com>
2023-01-17 18:01:52 +1100

    update

--- crowdfunding/projects/models.py
+++ crowdfunding/projects/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    title=models.CharField(max_length=200)
+    description=models.TextField()
+    goal=models.IntegerField()
+    image=models.URLField()
+    is_open=models.BooleanField()
+    date_created=models.DateTimeField(auto_now_add=True)
+    owner = models.CharField(max_length=200)
+        # will need to change this to ForeignKey after we created a user (from CharField)
+
+class Pledge(models.Model):
+    amount=models.IntegerField()
+    comment=models.CharField(max_length=300)
+    anonymous=models.BooleanField()
+    project=models.ForeignKey(
+        'Project',
+        on_delete=models.CASCADE,
+        related_name='pledges'
+    )
+    # will automatically create a project_id for the project
+    supporter = models.CharField(max_length=200)
